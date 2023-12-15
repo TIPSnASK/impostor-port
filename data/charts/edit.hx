8a84d0adc2bf453765fa546b976b1a62a5eeb41d
slime <85509162+seven-schmeckles@users.noreply.github.com>
2023-12-14 20:43:33 -0600

    doubles + impostorcam rewrite

--- data/charts/edit.hx
+++
@@ -1,6 +0,0 @@
-import funkin.editors.ui.UIState;
-
-function update() {
-	if (FlxG.keys.justPressed.THREE) FlxG.switchState(new UIState(true, 'editors/UICharacterEditor'));
-	if (FlxG.keys.justPressed.TWO) FlxG.switchState(new UIState(true, 'editors/UIStagePosEditor'));
-}
